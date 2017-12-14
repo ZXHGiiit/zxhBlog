dc3b39c5857447943b7994573d571da7c05af285
xinghang.zhou <13633843273@163.com>
2017-12-14 12:46:40 +0800

    个人博客框架搭建-SpringBoot

---
+++ src/main/java/com/zxh/Controller/DemoController.java
@@ -0,0 +1,21 @@
+package com.zxh.Controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+/**
+ * Created by admin on 2017/12/14.
+ */
+
+@Controller
+@RequestMapping("/demo")
+public class DemoController {
+
+    @RequestMapping("/hello")
+    @ResponseBody
+    public String hello() {
+        return "hello SpringBoot";
+    }
+
+}
