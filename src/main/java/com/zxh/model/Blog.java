a6b8041a03a44b910f6e5d82a3cfeec42def6494
xinghang.zhou <13633843273@163.com>
2017-12-14 21:53:52 +0800

    添加实体类Blog

---
+++ src/main/java/com/zxh/model/Blog.java
@@ -0,0 +1,134 @@
+package com.zxh.model;
+
+import javax.persistence.*;
+import java.util.Date;
+
+/**
+ * Created by admin on 2017/12/14.
+ */
+
+@Entity
+@Table(name = "blog")
+public class Blog {
+    @Id
+    @GeneratedValue
+    private Long id;
+    private String title;
+    private String content;
+    private String firstPic;
+    private String flag;
+    private Integer views;
+    private boolean appreciation;//赞赏是否开启
+    private boolean share;//分享是否开启
+    private boolean commentabled;//评论是否开启
+    private boolean published;//
+    private boolean recommend;
+    @Temporal(TemporalType.TIMESTAMP)
+    private Date createTime;
+    @Temporal(TemporalType.TIMESTAMP)
+    private Date updateTime;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+
+    public String getFirstPic() {
+        return firstPic;
+    }
+
+    public void setFirstPic(String firstPic) {
+        this.firstPic = firstPic;
+    }
+
+    public String getFlag() {
+        return flag;
+    }
+
+    public void setFlag(String flag) {
+        this.flag = flag;
+    }
+
+    public Integer getViews() {
+        return views;
+    }
+
+    public void setViews(Integer views) {
+        this.views = views;
+    }
+
+    public boolean isAppreciation() {
+        return appreciation;
+    }
+
+    public void setAppreciation(boolean appreciation) {
+        this.appreciation = appreciation;
+    }
+
+    public boolean isShare() {
+        return share;
+    }
+
+    public void setShare(boolean share) {
+        this.share = share;
+    }
+
+    public boolean isCommentabled() {
+        return commentabled;
+    }
+
+    public void setCommentabled(boolean commentabled) {
+        this.commentabled = commentabled;
+    }
+
+    public boolean isPublished() {
+        return published;
+    }
+
+    public void setPublished(boolean published) {
+        this.published = published;
+    }
+
+    public boolean isRecommend() {
+        return recommend;
+    }
+
+    public void setRecommend(boolean recommend) {
+        this.recommend = recommend;
+    }
+
+    public Date getCreateTime() {
+        return createTime;
+    }
+
+    public void setCreateTime(Date createTime) {
+        this.createTime = createTime;
+    }
+
+    public Date getUpdateTime() {
+        return updateTime;
+    }
+
+    public void setUpdateTime(Date updateTime) {
+        this.updateTime = updateTime;
+    }
+}
