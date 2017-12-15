71101afc699d2a5fb1031f2405aa559cc045c606
xinghang.zhou <13633843273@163.com>
2017-12-15 20:47:26 +0800

    添加admin拦截器

---
+++ src/main/java/com/zxh/config/WebConfig.java
@@ -0,0 +1,23 @@
+package com.zxh.config;
+
+import com.zxh.interceptor.LoginInterceptor;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+
+/**
+ * Created by admin on 2017/12/15.
+ */
+
+@Configuration
+public class WebConfig extends WebMvcConfigurerAdapter{
+
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+        registry.addInterceptor(new LoginInterceptor())
+                .addPathPatterns("/admin/**")
+                .excludePathPatterns("/admin")
+                .excludePathPatterns("/admin/login");
+    }
+
+}
---
+++ src/main/java/com/zxh/interceptor/LoginInterceptor.java
@@ -0,0 +1,21 @@
+package com.zxh.interceptor;
+
+import org.springframework.web.servlet.handler.HandlerInterceptorAdapter;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Created by admin on 2017/12/15.
+ * 登录拦截器，需要配置，见WebConfig
+ */
+public class LoginInterceptor extends HandlerInterceptorAdapter {
+    @Override
+    public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
+        if(request.getSession().getAttribute("user") == null) {
+            response.sendRedirect("/admin");
+            return false;//返回false，表示不往下执行，
+        }
+        return true;
+    }
+}
